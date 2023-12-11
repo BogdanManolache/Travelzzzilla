d07d5d9cb97d2929c09b0fcc136b8416c705eb4e
Bogdan2810 <121561815+Bogdan2810@users.noreply.github.com>
2023-12-11 17:43:26 +0200

    fixed styling issue with longer country names

--- app/favourites/[city]/CityGeography.jsx
+++ app/favourites/[city]/CityGeography.jsx
@@ -7,11 +7,17 @@ import { SlPeople } from 'react-icons/sl';
 import { TbMountain, TbWorldLatitude, TbWorldLongitude } from 'react-icons/tb';
 import Flag from 'react-world-flags';
 import Loader from '@/app/loading';
+import { notFound } from 'next/navigation';
+import { useEffect } from 'react';
 
-export default function CityGeography({ latitude, longitude }) {
+export async function generateStaticParams() {}
+
+export default function CityGeography({ latitude, longitude, name }) {
   const { cities } = useCities();
 
-  const city = cities.find(city => city.latitude === +latitude);
+  const city = cities.find(
+    city => city.name === name && city.latitude === +latitude,
+  );
 
   if (!city) return <Loader />;
 
@@ -21,30 +27,30 @@ export default function CityGeography({ latitude, longitude }) {
     <div className="mb-10 mt-2">
       <h4 className="mb-4 text-center font-semibold">Geography</h4>
       <ul className="flex flex-col items-center gap-4 p-4 text-sm lg:flex-row">
-        <li className="flex w-full flex-col items-center gap-2 rounded-sm bg-slate-100 p-4 shadow-md lg:flex-1">
+        <li className="flex w-full flex-col items-center gap-3 self-stretch rounded-sm bg-slate-100 p-4 shadow-md lg:flex-1">
           <HiOutlineFlag className="stroke-slate-500" size={40} />
-          <div className="flex items-center gap-2">
+          <div className="flex flex-wrap items-center gap-2">
             <span>{country}</span>
             <Flag as="image" code={country_code} className="h-4" />
           </div>
         </li>
         {population && (
-          <li className="flex w-full flex-col items-center gap-2 rounded-sm bg-slate-100 p-4 shadow-md lg:flex-1">
+          <li className="flex w-full flex-col items-center gap-3 self-stretch rounded-sm bg-slate-100 p-4 shadow-md lg:flex-1">
             <SlPeople className="fill-slate-500" size={40} />
             <span>{population}</span>
           </li>
         )}
-        <li className="flex w-full items-center justify-center gap-4 rounded-sm bg-slate-100 p-4 shadow-md lg:flex-1">
-          <div className="flex flex-col items-center gap-2">
+        <li className="flex w-full items-start justify-center gap-4 self-stretch rounded-sm bg-slate-100 p-4 shadow-md lg:flex-1">
+          <div className="flex flex-col items-center gap-3">
             <TbWorldLatitude className="stroke-slate-500" size={40} />
             <span>{latitude}&deg;</span>
           </div>
-          <div className="flex flex-col items-center gap-2">
+          <div className="flex flex-col items-center gap-3">
             <TbWorldLongitude className="stroke-slate-500" size={40} />
             <span>{longitude}&deg;</span>
           </div>
         </li>
-        <li className="flex w-full flex-col items-center gap-2 rounded-sm bg-slate-100 p-4 shadow-md lg:flex-1">
+        <li className="flex w-full flex-col items-center gap-3 self-stretch rounded-sm bg-slate-100 p-4 shadow-md lg:flex-1">
           <TbMountain className="stroke-slate-500" size={40} />
           <span>{elevation} m</span>
         </li>
--- app/favourites/[city]/page.jsx
+++ app/favourites/[city]/page.jsx
@@ -13,14 +13,13 @@ export default function CityPage({ params, searchParams }) {
   const { city: name } = params;
   const { lat: latitude, long: longitude } = searchParams;
 
-  if (!name) notFound();
-  if (!latitude || !longitude) notFound();
+  if (!name || !latitude || !longitude) notFound();
 
   return (
     <div className="relative mx-auto flex w-5/6 flex-col px-4 py-2">
       <h2 className="mt-2 text-center">Stats about {decodeURI(name)}</h2>
       <CityDelete name={name} latitude={latitude} longitude={longitude} />
-      <CityGeography latitude={latitude} longitude={longitude} />
+      <CityGeography name={name} latitude={latitude} longitude={longitude} />
       <CityWeather latitude={latitude} longitude={longitude} />
     </div>
   );
