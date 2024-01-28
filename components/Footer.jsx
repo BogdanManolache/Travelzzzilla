0c0cbf95ab7855c8ca6b5676402892e800547778
Bogdan Manolache <121561815+BogdanManolache@users.noreply.github.com>
2024-01-28 23:28:38 +0200

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="col-span-2 flex items-center justify-center border-t-2 md:border-none">
       <p className="text-sm font-light text-slate-700">
-        &copy; Travelzilla, Inc. {new Date().getFullYear()}
+        &copy; Travelzzzilla, Inc. {new Date().getFullYear()}
       </p>
     </footer>
   );
