2912a6c728fda536df9c86809470f97283d620f3
Bogdan2810 <121561815+Bogdan2810@users.noreply.github.com>
2024-03-25 01:27:18 +0200

    changed header styling after sm breakpoint

--- components/Header.jsx
+++ components/Header.jsx
@@ -19,7 +19,9 @@ export default async function Header() {
     <header className="col-span-2 col-start-1 flex items-center justify-between border-b-2 pl-6 sm:col-span-1  sm:col-start-1 md:col-span-2 md:col-start-1 md:border-none">
       <Link href="/" className="flex items-center">
         <Image src="/logo.png" width={64} height={64} alt="Logo" />
-        <span className={`${barlowCondensed.className} text-4xl  font-bold`}>
+        <span
+          className={`${barlowCondensed.className} inline-block text-2xl font-bold sm:hidden  md:inline-block md:text-4xl`}
+        >
           Travelzzzilla
         </span>
       </Link>
